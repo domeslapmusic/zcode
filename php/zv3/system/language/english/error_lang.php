8b5837e8a45c021d9e579d2171ad27a3b87106d5
zzzarate <zzzarate@1bddb8f6-7a2d-0410-8919-192e16dc3443>
2008-03-30 01:32:09 +0000

    Now handling 404 errors inside CodeIgniter

---
+++ php/zv3/system/language/english/error_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['error_notfound'] = "Ummm... cannot find what you're looking for, I'm afraid. Could you please try with one of the links in the menu? Thanks!";
+
+?>
---
+++ php/zv3/system/language/spanish/error_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['error_notfound'] = "Ummm... lo siento pero no encuentro lo que buscas. &iquest;Te importar&iacute;a probar con alguno de los enlaces del men&uacute;? Gracias!";
+
+?>

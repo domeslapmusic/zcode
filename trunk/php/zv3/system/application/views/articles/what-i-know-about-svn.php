13401dc6063cd6c399ec744f44e78e76478f8bf7
zzzarate <zzzarate@1bddb8f6-7a2d-0410-8919-192e16dc3443>
2008-07-31 10:24:38 +0000

    Uploading first version of What i know about SVN. HTH.

--- trunk/php/zv3/system/application/views/articles/what-i-know-about-svn.php
+++ trunk/php/zv3/system/application/views/articles/what-i-know-about-svn.php
@@ -1,3 +1,2 @@
-<p><strong>What I know about SVN</strong> is an introduction to SVN for beginners/average users that I prepared to support my talk <a href="http://www.lfpug.com/31st-july-2008-31072008/">SVN for Flash at LFPUG on July 2008</a>.</p>
+<p><strong>What I know about SVN</strong> is an introduction to SVN for beginners/average users that I prepared to support my talk <a href="http://www.lfpug.com/31st-july-2008-31072008/">SVN for Flash at LFPUG on July 2008</a>. You can <a href="<?php echo base_url(); ?>assets/articles/what-i-know-about-svn/What-I-know-about-SVN.pdf">download it as PDF</a>.</p>
 <p>Once translated it will be part of the world famous and internationally acclaimed series <a href="http://www.loqueyosede.com/">Lo que yo s&eacute; de</a> ("What I know about").</p>
-<p>Stay tunned for more info.</p>
